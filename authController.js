ca87635ae24aaaf0536c29662b241a1c87abc1ef
Khimish009 <salman.khimishev01@gmail.com>
2023-10-09 22:23:38 +0300

    feat: registration

---
+++ authController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt')
+const User = require('./models/User')
+const Role = require('./models/Role')
+
+class AuthController {
+    async registration(req, res) {
+        try {
+            const { username, password } = req.body
+            const candidate = await User.findOne({ username })
+
+            if (candidate) {
+                return res.status(400).json({ message: 'Пользователь с таким именем существует!' })
+            }
+
+            const hashPassword = bcrypt.hashSync(password, 7)
+            const userRole = await Role.findOne({ value: 'USER' })
+            const user = new User({ username, password: hashPassword, roles: [userRole.value] })
+            await user.save()
+            return res.json({ message: 'Пользователь успешно зарегистрирован!' })
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({ message: "Registration error" })
+        }
+    }
+
+    async login(req, res) {
+        try {
+
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({ message: "Login error" })
+        }
+    }
+
+    async getUsers(req, res) {
+        try {
+            res.json('server works!!!')
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
+
+module.exports = new AuthController()
